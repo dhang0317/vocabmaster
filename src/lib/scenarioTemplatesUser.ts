590756c28a26f64e4c0843180e285a4b3b587ac3
dhang0317 <jeffery0990317@gmail.com>
2026-08-26 01:12:20 +0800

    chore: wire C0+C1 into USER templates (batch2 upload in progress)

--- src/lib/scenarioTemplatesUser.ts
+++ src/lib/scenarioTemplatesUser.ts
@@ -3,11 +3,15 @@ import { USER_SCENARIO_TEMPLATES_A0 } from './scenarioTemplatesUserA0';
 import { USER_SCENARIO_TEMPLATES_A1 } from './scenarioTemplatesUserA1';
 import { USER_SCENARIO_TEMPLATES_B0 } from './scenarioTemplatesUserB0';
 import { USER_SCENARIO_TEMPLATES_B1 } from './scenarioTemplatesUserB1';
+import { USER_SCENARIO_TEMPLATES_C0 } from './scenarioTemplatesUserC0';
+import { USER_SCENARIO_TEMPLATES_C1 } from './scenarioTemplatesUserC1';
 
-/** Full user-expanded set (60 templates) */
+/** User-expanded set (partial while batch2 uploads) */
 export const USER_SCENARIO_TEMPLATES: ScenarioTemplate[] = [
   ...USER_SCENARIO_TEMPLATES_A0,
   ...USER_SCENARIO_TEMPLATES_A1,
   ...USER_SCENARIO_TEMPLATES_B0,
   ...USER_SCENARIO_TEMPLATES_B1,
+  ...USER_SCENARIO_TEMPLATES_C0,
+  ...USER_SCENARIO_TEMPLATES_C1,
 ];
