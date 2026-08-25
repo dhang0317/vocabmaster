ac5c107343940e23c5c511d7d2d465f9131168c8
dhang0317 <jeffery0990317@gmail.com>
2026-08-26 01:22:36 +0800

    feat: add C12 + wire full USER set (60+132=192 user templates)

--- src/lib/scenarioTemplatesUser.ts
+++ src/lib/scenarioTemplatesUser.ts
@@ -9,8 +9,16 @@ import { USER_SCENARIO_TEMPLATES_C2 } from './scenarioTemplatesUserC2';
 import { USER_SCENARIO_TEMPLATES_C3 } from './scenarioTemplatesUserC3';
 import { USER_SCENARIO_TEMPLATES_C4 } from './scenarioTemplatesUserC4';
 import { USER_SCENARIO_TEMPLATES_C5 } from './scenarioTemplatesUserC5';
+import { USER_SCENARIO_TEMPLATES_C6 } from './scenarioTemplatesUserC6';
+import { USER_SCENARIO_TEMPLATES_C7 } from './scenarioTemplatesUserC7';
+import { USER_SCENARIO_TEMPLATES_C8 } from './scenarioTemplatesUserC8';
+import { USER_SCENARIO_TEMPLATES_C9 } from './scenarioTemplatesUserC9';
+import { USER_SCENARIO_TEMPLATES_C10 } from './scenarioTemplatesUserC10';
+import { USER_SCENARIO_TEMPLATES_C11 } from './scenarioTemplatesUserC11';
+import { USER_SCENARIO_TEMPLATES_C12 } from './scenarioTemplatesUserC12';
+import { USER_SCENARIO_TEMPLATES_C13 } from './scenarioTemplatesUserC13';
 
-/** User-expanded set (batch2: C0-C5 live; C6-C12 uploading) */
+/** Full user-expanded set (60 prior + 132 batch2 = 192) */
 export const USER_SCENARIO_TEMPLATES: ScenarioTemplate[] = [
   ...USER_SCENARIO_TEMPLATES_A0,
   ...USER_SCENARIO_TEMPLATES_A1,
@@ -22,4 +30,12 @@ export const USER_SCENARIO_TEMPLATES: ScenarioTemplate[] = [
   ...USER_SCENARIO_TEMPLATES_C3,
   ...USER_SCENARIO_TEMPLATES_C4,
   ...USER_SCENARIO_TEMPLATES_C5,
+  ...USER_SCENARIO_TEMPLATES_C6,
+  ...USER_SCENARIO_TEMPLATES_C7,
+  ...USER_SCENARIO_TEMPLATES_C8,
+  ...USER_SCENARIO_TEMPLATES_C9,
+  ...USER_SCENARIO_TEMPLATES_C10,
+  ...USER_SCENARIO_TEMPLATES_C11,
+  ...USER_SCENARIO_TEMPLATES_C12,
+  ...USER_SCENARIO_TEMPLATES_C13,
 ];
